8538f4e93ac0f2776ce313f3b211b1450e88fa5a
AlexisDabon <xela619375@gmail.com>
2026-06-18 15:49:40 +0200

    array/for

---
+++ crypto_alert.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+float calculer_moyenne (float p[], int size);
+int main () {
+	
+	float prix [3];
+	for (int i = 0; i < 3; i++) {
+		printf("saisissez le prix %d", i + 1);
+		scanf("%f", &prix[i]); 
+	}
+
+
+}
+
+
