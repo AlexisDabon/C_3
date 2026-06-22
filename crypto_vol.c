8ccb5f201b0b1d655508ccac301a3c081b05f7cb
AlexisDabon <xela619375@gmail.com>
2026-06-22 10:54:10 +0200

    for

--- crypto_vol.c
+++ crypto_vol.c
@@ -6,7 +6,10 @@ int main () {
 	float prix [3];
 	float ecart = 0;
 	int alert = 0;
-
-
+	
+	for (int i = 0; i < 3; i++) {
+	printf("saisissez le prix %d : \n", i + 1);
+	scanf("%f", &prix [i]);
+	}
 
 }
