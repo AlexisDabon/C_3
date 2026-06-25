031716438e3d959e20ac4652ec4e0df546fe4907
AlexisDabon <xela619375@gmail.com>
2026-06-25 17:19:10 +0200

    exercise completed

--- crypto_id.c
+++ crypto_id.c
@@ -15,3 +15,13 @@ int main () {
 	printf("prix de la crypto %d : %.2f \n", ma_crypto.id, ma_crypto.prix);
 
 	mettre_prix_a_jour(&ma_crypto, 48500.50);
+
+	printf("nouveau prix %d : %.2f \n", ma_crypto.id, ma_crypto.prix);
+
+return 0;
+
+}
+
+void mettre_prix_a_jour(struct Crypto *c, float nouveau_prix) {
+c->prix = nouveau_prix;
+}
