cd75b8e311ae22aa590d20e5a956e25aa4596e9d
AlexisDabon <xela619375@gmail.com>
2026-06-25 17:04:57 +0200

    main

--- crypto_id.c
+++ crypto_id.c
@@ -6,3 +6,12 @@ struct Crypto {
 };
 
 void mettre_prix_a_jour(struct Crypto *c, float nouveau_prix);
+
+int main () {
+	struct Crypto ma_crypto; 
+	ma_crypto.id = 1;
+	ma_crypto.prix = 45000.00;
+	
+	printf("prix de la crypto %d : %.2f \n", ma_crypto.id, ma_crypto.prix);
+
+	mettre_prix_a_jour(&ma_crypto, 48500.50);
