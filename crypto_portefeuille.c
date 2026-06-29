96a549feee91c6faa11d3654222e6dacbd89d2b9
AlexisDabon <xela619375@gmail.com>
2026-06-29 06:57:47 +0200

    feat: main

--- crypto_portefeuille.c
+++ crypto_portefeuille.c
@@ -6,3 +6,10 @@ struct Crypto {
 };
 
 void afficher_portefeuille(struct Crypto t[], int taille);
+
+int main() {
+	struct Crypto portefeuille[3] = {{1, 45000.0}, {2, 3200.0}, {3, 1.20}};
+	afficher_portefeuille (portefeuille, 3);
+
+	return 0;
+}
